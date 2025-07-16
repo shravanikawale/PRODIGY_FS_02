48405eddea618abf205875ae8382cc498d3f78f7
shravanikawale <shravanikawale09@gmail.com>
2025-07-16 21:01:07 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/employees");
+
+app.use('/api/auth', require('./backend/routes/auth'));
+app.use('/api/employees', require('./backend/routes/employees'));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
+
